b8b7e872bd423bbed727e5d8452ed46fed24162f
Ricky Nandha <ricky.nandha2@gmail.com>
2025-05-02 13:54:19 +0100

    monorepo project

---
+++ client/src/main.js
@@ -0,0 +1,38 @@
+console.log("projectt");
+
+async function getPosts() {
+  console.log("I am getPosts and I am about tp fetch data");
+  const response = await fetch("http://localhost:8080/books");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  console.log("I am getPosts amd about to give data to renderDataOntoPage");
+  renderDataOntoPage(data);
+}
+
+function renderDataOntoPage(apiResults) {
+  console.log("I am renderDataOntoPage and about to loop throught the array");
+  const resultsDiv = document.getElementById("results");
+
+  resultsDiv.innerHTML = "";
+
+  for (let index = 0; index < apiResults.length; index++) {
+    const element = apiResults[index];
+    console.log(element);
+
+    const newTitle = document.createElement("h2");
+    const newBody = document.createElement("p");
+
+    newTitle.textContent = element.title;
+
+    newBody.textContent = element.body;
+
+    resultsDiv.appendChild(newTitle);
+    resultsDiv.appendChild(newBody);
+  }
+}
+const postsBtn = document.getElementById("get-posts-button");
+postsBtn.addEventListener("click", function () {
+  console.log("I am about to call getPosts");
+  getPosts();
+});
---
+++ server/server.js
@@ -0,0 +1,49 @@
+import express from "express";
+
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const books = [
+  {
+    title: "New York",
+    body: "is 3,392 miles away",
+  },
+  {
+    title: "London",
+    body: "is 89 miles away",
+  },
+  {
+    title: "Dubai",
+    body: "is 3,460 miles away",
+  },
+
+  {
+    title: "Cairo",
+    body: "is 3,607 miles away",
+  },
+
+  {
+    title: "Rome",
+    body: "is 977 miles away",
+  },
+  {
+    title: "Madrid",
+    body: "is 853 miles away",
+  },
+];
+
+app.get("/", function (req, res) {
+  res.json("Who invited you here! Show your ticket please!");
+});
+
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+const PORT = 8080;
+
+app.listen(PORT, function (req, res) {
+  console.log(`Server is on port ${PORT}`);
+});
